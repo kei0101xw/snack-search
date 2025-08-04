3f3e448b2fde325b110f26cb80d1c7857f76519f
kei0101xw <kei0101xw@icloud.com>
2025-08-04 21:16:51 +0900

    非同期でデータを取ってくるようにした

--- MyOkashi/OkashiData.swift
+++ MyOkashi/OkashiData.swift
@@ -34,5 +34,14 @@ import SwiftUI
         }
         
         print(req_url)
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: req_url)
+            let decoder = JSONDecoder()
+            let json = try decoder.decode(ResultJson.self, from: data)
+            print(json)
+        } catch {
+            print("エラーが出ました")
+        }
     }
 }
