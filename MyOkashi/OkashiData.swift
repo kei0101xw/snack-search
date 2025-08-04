18755ca114a7f371415825d0d3d8b9899862bec1
kei0101xw <kei0101xw@icloud.com>
2025-08-05 00:59:07 +0900

    jsonデータを配列に格納するようにした

--- MyOkashi/OkashiData.swift
+++ MyOkashi/OkashiData.swift
@@ -33,6 +33,7 @@ struct OkashiItem: Identifiable {
         }
     }
     
+    @MainActor
     private func search(keyword: String) async {
         guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
             return
@@ -48,7 +49,27 @@ struct OkashiItem: Identifiable {
             let (data, _) = try await URLSession.shared.data(from: req_url)
             let decoder = JSONDecoder()
             let json = try decoder.decode(ResultJson.self, from: data)
-            print(json)
+//            print(json)
+            
+            guard let items = json.item else {
+                return
+            }
+            
+            okashiList.removeAll()
+            
+            for item in items {
+                if let name = item.name,
+                   let link = item.url,
+                   let image = item.image {
+                    let okashi = OkashiItem(name: name, link: link, image: image)
+                    
+                    okashiList.append(okashi)
+                }
+                
+            }
+            
+            print(okashiList)
+            
         } catch {
             print("エラーが出ました")
         }
