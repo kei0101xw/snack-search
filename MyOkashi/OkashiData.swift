c17356cbabee65bcb31fec7b0ba47a3c8bc2dec5
kei0101xw <kei0101xw@icloud.com>
2025-08-04 17:29:56 +0900

    お菓子のデータを取得するOkashiData.swiftを追加

---
+++ MyOkashi/OkashiData.swift
@@ -0,0 +1,14 @@
+//
+//  OkashiData.swift
+//  MyOkashi
+//
+//  Created by 原田佳祐 on 2025/08/04.
+//
+
+import SwiftUI
+
+@Observable class OkashiData {
+    func searchOkashi(keyword: String) {
+        print("searchOkashiメソッドで受け取った値：\(keyword)")
+    }
+}
