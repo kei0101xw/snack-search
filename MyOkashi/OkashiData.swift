423c1d7ad91f4bbd966d2d4335948c43f3a3530f
kei0101xw <kei0101xw@icloud.com>
2025-08-04 19:50:27 +0900

    お菓子検索の非同期処理の雛形を記述

--- MyOkashi/OkashiData.swift
+++ MyOkashi/OkashiData.swift
@@ -10,5 +10,13 @@ import SwiftUI
 @Observable class OkashiData {
     func searchOkashi(keyword: String) {
         print("searchOkashiメソッドで受け取った値：\(keyword)")
+        
+        Task {
+            await search(keyword: keyword)
+        }
+    }
+    
+    private func search(keyword: String) async {
+        print("searchメソッドで受け取った値：\(keyword)")
     }
 }
