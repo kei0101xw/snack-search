962264e6fd4f421cce6998acca16c1e29c2fbc3a
kei0101xw <kei0101xw@icloud.com>
2025-08-05 01:38:08 +0900

    Safari表示画面を作成

---
+++ MyOkashi/SafariView.swift
@@ -0,0 +1,18 @@
+//
+//  SafariView.swift
+//  MyOkashi
+//
+//  Created by 原田佳祐 on 2025/08/05.
+//
+
+import SwiftUI
+
+struct SafariView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SafariView()
+}
